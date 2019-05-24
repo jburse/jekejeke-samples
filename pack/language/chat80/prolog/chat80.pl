268338e33d3ca7b10c35662f44bae50d75b90c37
Jan Burse <janburse@fastmail.fm>
2019-05-24 13:40:42 +0200

    Launch Jekejeke Prolog 1.3.7

--- pack/language/chat80/prolog/chat80.pl
+++ pack/language/chat80/prolog/chat80.pl
@@ -45,6 +45,9 @@
  */
 
 :- module(chat80, [hi/0]).
+
+:- current_prolog_flag(dialect, jekejeke)
+-> use_module(library(edinburgh)); true.
 :- current_prolog_flag(dialect, jekejeke)
 -> use_module(library(preprocessor)); true.
 :- use_module(readin).
@@ -89,10 +92,10 @@ ask(user, P) :- !,
 %    advance(X,N0,N),
 %    doing(L,N).
 %
-% out(nb(X)) :- !,
-%    write(X).
-% out(A) :-
-%    write(A).
+out(nb(X)) :- !,
+   write(X).
+out(A) :-
+   write(A).
 %
 % advance(X,N0,N) :-
 %    uses(X,K),
@@ -125,12 +128,12 @@ control([trace,'.']) :- !,
 control([do,not,trace,'.']) :- !,
    tracing ~= off,
    write('No longer tracing.'), nl, fail.
-% control([do,mini,demo,'.']) :- !,
-%    write('Executing mini demo...'), nl,
-%    demo(mini), fail.
-% control([do,main,demo,'.']) :- !,
-%    write('Executing main demo...'), nl,
-%    demo(main), fail.
+control([do,mini,demo,'.']) :- !,
+   write('Executing mini demo...'), nl,
+   demo(mini), fail.
+control([do,main,demo,'.']) :- !,
+   write('Executing main demo...'), nl,
+   demo(main), fail.
 % control([test,chat,'.']) :- !,
 %    test_chat, fail.
 control(U0) :-
@@ -196,3 +199,144 @@ uptime(T) :-
    statistics(walltime, [T|_]).
 
 :- endif.
+
+/* ----------------------------------------------------------------------
+	Simple questions
+	These question do not require setof/3 and are useful for early
+	testing of a system.
+   ---------------------------------------------------------------------- */
+
+eg([does,america,contain,new_york,?]).
+eg([does,mexico,border,the,united_states,?]).
+eg([is,the,population,of,china,greater,than,nb(200),million,?]).
+eg([does,the,population,of,china,exceed,nb(1000),million,?]).
+eg([is,the,population,of,china,nb(840),million,?]).
+eg([does,the,population,of,china,exceed,the,population,of,india,?]).
+eg([is,spain,bordered,by,the,pacific,?]).
+eg([does,the,atlantic,border,spain,?]).
+eg([is,the,rhine,in,switzerland,?]).
+eg([is,the,united_kingdom,in,europe,?]).
+
+/* ----------------------------------------------------------------------
+	Standard question set
+	This is the standard chat question set, originally put together
+	by David and Fernando and use in their papers. Quintus uses this
+	set as a standard for performance comparisons.
+   ---------------------------------------------------------------------- */
+
+ed(1, [what,rivers,are,there,?],
+
+   [amazon,amu_darya,amur,brahmaputra,colorado,congo_river,cubango,danube,don,elbe,euphrates,ganges,hwang_ho,indus,irrawaddy,lena,limpopo,mackenzie,mekong,mississippi,murray,niger_river,nile,ob,oder,orange,orinoco,parana,rhine,rhone,rio_grande,salween,senegal_river,tagus,vistula,volga,volta,yangtze,yenisei,yukon,zambesi]).
+
+ed(2, [does,afghanistan,border,china,?],
+
+   [true]).
+
+ed(3, [what,is,the,capital,of,upper_volta,?],
+
+   [ouagadougou]).
+
+ed(4, [where,is,the,largest,country,?],
+
+   [asia,northern_asia]).
+
+ed(5, [which,countries,are,european,?],
+
+   [albania,andorra,austria,belgium,bulgaria,cyprus,czechoslovakia,denmark,east_germany,eire,finland,france,greece,hungary,iceland,italy,liechtenstein,luxembourg,malta,monaco,netherlands,norway,poland,portugal,romania,san_marino,spain,sweden,switzerland,united_kingdom,west_germany,yugoslavia]).
+
+ed(6, [which,country,'''',s,capital,is,london,?],
+
+   [united_kingdom]).
+
+ed(7, [which,is,the,largest,african,country,?],
+
+   [sudan]).
+
+ed(8, [how,large,is,the,smallest,american,country,?],
+
+   [0--ksqmiles]).
+
+ed(9, [what,is,the,ocean,that,borders,african,countries,and,that,borders,asian,countries,?],
+
+   [indian_ocean]).
+
+ed(10, [what,are,the,capitals,of,the,countries,bordering,the,baltic,?],
+
+   [[[denmark]:[copenhagen],[east_germany]:[east_berlin],[finland]:[helsinki],[poland]:[warsaw],[soviet_union]:[moscow],[sweden]:[stockholm],[west_germany]:[bonn]]]).
+
+ed(11, [which,countries,are,bordered,by,two,seas,?],
+
+   [egypt,iran,israel,saudi_arabia,turkey]).
+
+ed(12, [how,many,countries,does,the,danube,flow,through,?],
+
+   [6]).
+
+ed(13, [what,is,the,total,area,of,countries,south,of,the,equator,and,not,in,australasia,?],
+
+   [10228--ksqmiles]).
+
+ed(14, [what,is,the,average,area,of,the,countries,in,each,continent,?],
+
+   [[africa,233--ksqmiles],[america,496--ksqmiles],[asia,485--ksqmiles],[australasia,543--ksqmiles],[europe,58--ksqmiles]]).
+
+ed(15, [is,there,more,than,one,country,in,each,continent,?],
+
+   [false]).
+
+ed(16, [is,there,some,ocean,that,does,not,border,any,country,?],
+
+   [true]).
+
+ed(17, [what,are,the,countries,from,which,a,river,flows,into,the,black_sea,?],
+
+   [[romania,soviet_union]]).
+
+ed(18, [what,are,the,continents,no,country,in,which,contains,more,than,two,cities,whose,population,exceeds,nb(1),million,?],
+
+   [[africa,antarctica,australasia]]).
+
+ed(19, [which,country,bordering,the,mediterranean,borders,a,country,that,is,bordered,by,a,country,whose,population,exceeds,the,population,of,india,?],
+
+   [turkey]).
+
+ed(20, [which,countries,have,a,population,exceeding,nb(10),million,?],
+
+   [afghanistan,algeria,argentina,australia,bangladesh,brazil,burma,canada,china,colombia,czechoslovakia,east_germany,egypt,ethiopia,france,india,indonesia,iran,italy,japan,kenya,mexico,morocco,nepal,netherlands,nigeria,north_korea,pakistan,peru,philippines,poland,south_africa,south_korea,soviet_union,spain,sri_lanka,sudan,taiwan,tanzania,thailand,turkey,united_kingdom,united_states,venezuela,vietnam,west_germany,yugoslavia,zaire]).
+
+ed(21, [which,countries,with,a,population,exceeding,nb(10),million,border,the,atlantic,?],
+
+   [argentina,brazil,canada,colombia,france,mexico,morocco,netherlands,nigeria,south_africa,spain,united_kingdom,united_states,venezuela,west_germany,zaire]).
+
+ed(22, [what,percentage,of,countries,border,each,ocean,?],
+
+   [[arctic_ocean,2],[atlantic,35],[indian_ocean,14],[pacific,20]]).
+
+ed(23, [what,countries,are,there,in,europe,?],
+
+   [albania,andorra,austria,belgium,bulgaria,cyprus,czechoslovakia,denmark,east_germany,eire,finland,france,greece,hungary,iceland,italy,liechtenstein,luxembourg,malta,monaco,netherlands,norway,poland,portugal,romania,san_marino,spain,sweden,switzerland,united_kingdom,west_germany,yugoslavia]).
+
+/* ----------------------------------------------------------------------
+	Simple Access to demonstrations
+   ---------------------------------------------------------------------- */
+
+demo(Type) :-
+   demo(Type, L),
+   inform(L),
+   check_words(L, S),
+   process(S).
+
+demo(mini, List) :-
+   eg(List).
+demo(main, List) :-
+   ed(_, List, _).
+
+inform(L) :-
+   write('Question: '),
+   inform1(L), nl.
+
+inform1([]).
+inform1([H|T]) :-
+   out(H),
+   put(" "),
+   inform1(T).
