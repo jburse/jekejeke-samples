f62d67fb4703e85e5d6857a44b9150060d318540
Jan Burse <janburse@fastmail.fm>
2020-09-20 13:22:52 +0200

    Launch Jekejeke Prolog 1.4.6

--- jekrun/deployweb/example04/Applet.java
+++ jekrun/deployweb/example04/Applet.java
@@ -1,5 +1,7 @@
-package example03;
+package example04;
 
+import example01.Data;
+import example01.Query;
 import jekpro.platform.headless.ToolkitLibrary;
 import jekpro.tools.call.Interpreter;
 import jekpro.tools.call.InterpreterException;
@@ -7,8 +9,6 @@ import jekpro.tools.call.InterpreterMessage;
 import jekpro.tools.term.AbstractTerm;
 import jekpro.tools.term.Knowledgebase;
 import jekpro.tools.term.TermVar;
-import example02.Pane;
-import example01.Query;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -48,8 +48,11 @@ import java.io.StringWriter;
  * Jekejeke is a registered trademark of XLOG Technologies GmbH.
  */
 public final class Applet extends JApplet implements ActionListener {
-    private Knowledgebase know = new Knowledgebase(ToolkitLibrary.DEFAULT);
-    private Pane pane = new Pane();
+    private final Pane pane = new Pane();
+
+    /**
+     * appletviewer -J-Djava.security.policy=<path>\example04\applet.policy <path>\example04\page.html
+     */
 
     /**
      * <p>Setup the knowledgebase and init the pane.</p>
@@ -74,16 +77,16 @@ public final class Applet extends JApplet implements ActionListener {
      */
     private void initKnowledgebase() {
         try {
-            /* setup the Prolog runtime */
-            Interpreter inter = know.iterable();
-            Knowledgebase.initKnowledgebase(inter);
-            /* load the Prolog code */
-            Object consultGoal = inter.parseTerm("consult(library(example01/table))");
-            inter.iterator(consultGoal).next().close();
-        } catch (InterpreterMessage x) {
-            throw new RuntimeException(x);
-        } catch (InterpreterException x) {
-            throw new RuntimeException(x);
+            Data.getKnowledgebase();
+        } catch (Exception x) {
+            /* simple problem exception handling */
+            StringWriter sr = new StringWriter();
+            x.printStackTrace(new PrintWriter(sr));
+            JTextArea textarea = new JTextArea(sr.toString(), 8, 40);
+            textarea.setLineWrap(true);
+            JOptionPane.showMessageDialog(this,
+                    new JScrollPane(textarea),
+                    "Problem Report", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -94,7 +97,9 @@ public final class Applet extends JApplet implements ActionListener {
      */
     public void actionPerformed(ActionEvent e) {
         try {
+            Knowledgebase know = Data.getKnowledgebase();
             Interpreter inter = know.iterable();
+
             final Query query = new Query(inter);
             /* retrieve the search criteria and build the query */
             query.setFirstname(pane.getFirstName());
