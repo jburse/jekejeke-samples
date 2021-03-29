d9653bf3112efd797d53f154780a5dc5c24072ec
Jan Burse <janburse@fastmail.fm>
2021-03-29 23:30:49 +0200

    Launch Jekejeke Prolog 1.5.1

--- jekrun/deployweb/example01/Plain.java
+++ jekrun/deployweb/example01/Plain.java
@@ -4,9 +4,9 @@ import jekpro.tools.call.Interpreter;
 import jekpro.tools.term.Knowledgebase;
 import jekpro.tools.term.TermVar;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
